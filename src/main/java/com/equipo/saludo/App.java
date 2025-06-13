693b64bf4004e6f2c4e2bbece211418da2af1fa0
Christopher <cgonzalez@argonsolutions.cl>
2025-06-12 22:27:37 -0400

    corrigiendo version junit

--- src/main/java/com/equipo/saludo/App.java
+++ src/main/java/com/equipo/saludo/App.java
@@ -1,13 +1,9 @@
 package com.equipo.saludo;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+ public static String saludar(String nombre) {
+ return "¡Hola, " + nombre + "!";
+ }
+ public static void main(String[] args) {
+ System.out.println(saludar("DevOps"));
+ }
 }
